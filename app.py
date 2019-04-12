499bc44b8f2c0f99d38634097241b613881fe383
Brendo Bomfim <brendobomfim@sciosolutions.com.br>
2019-04-12 15:39:23 -0300

    change the ident from space to tabs

--- app.py
+++ app.py
@@ -8,23 +8,22 @@ import upload_files
 import json
 import logging
 
-#Number used on Hermes
+# Number used on Hermes
 recipient = 558588886522
 
-logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG, filename= "logs/" + str(recipient) + '_hermes_integration.log',
-                    filemode='w', datefmt='%d/%m/%Y %H:%M:%S')
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG,
+					filename="logs/" + str(recipient) + '_hermes_integration.log',
+					filemode='w', datefmt='%d/%m/%Y %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-#Hermes credentials
+# Hermes credentials
 api_username = "scio"
 api_password = "Sc1o"
 media_id = "ffbff03b-d193-45ac-a28c-625568e9cd86"
 
 omni_link = "http://localhost"
 omni_port = "3000"
-
-
 
 bot = Bot(api_username, api_password)
 
@@ -41,22 +40,22 @@ def receive_message():
 
 		Expects the JSON to be like:
 		{
-		    "messages": [
-		        {
-		            "from": "1234567890",
-		            "id": "ABGGhSkIc2B_Ago-sDy5BNm1gI5",
-		            "text": {
-		                "body": "Hi"
-		            },
-		            "timestamp": "1529381066",
-		            "type": "text"
-		        }
-		    ],
-		    "media_id": "xxx-xxx-xxxx"
+			"messages": [
+				{
+					"from": "1234567890",
+					"id": "ABGGhSkIc2B_Ago-sDy5BNm1gI5",
+					"text": {
+						"body": "Hi"
+					},
+					"timestamp": "1529381066",
+					"type": "text"
+				}
+			],
+			"media_id": "xxx-xxx-xxxx"
 		}
 
-	    Returns:
-	      The sucess of error from omni request
+		Returns:
+		  The sucess of error from omni request
 	"""
 
 	response = ""
@@ -77,10 +76,10 @@ def receive_message():
 				base64_string = download_file(url_name)
 
 				payload = {'sender': message.get('from'), 'recipient': recipient,
-				           'send_date':frmt_date, 'media': 'media',
-				           'media_type':  message.get('type'), 'plataform_name': 'whatsapp_business',
-	                       'media_name': file_name, 'content': base64_string,
-	                       'message_id': message.get('id')}
+						   'send_date': frmt_date, 'media': 'media',
+						   'media_type': message.get('type'), 'plataform_name': 'whatsapp_business',
+						   'media_name': file_name, 'content': base64_string,
+						   'message_id': message.get('id')}
 
 				logger.debug(f"receive_message -> message -> file -> payload: {payload}")
 
@@ -90,8 +89,8 @@ def receive_message():
 
 			elif "text" in message:
 				payload = {'sender': message.get('from'), 'recipient': recipient,
-				           'send_date': frmt_date, 'content': message.get('text').get('body'),
-				           'message_id': message.get('id'), 'plataform_name': 'whatsapp_business'}
+						   'send_date': frmt_date, 'content': message.get('text').get('body'),
+						   'message_id': message.get('id'), 'plataform_name': 'whatsapp_business'}
 
 				logger.debug(f"receive_message -> message -> text -> payload: {payload}")
 
@@ -107,6 +106,7 @@ def receive_message():
 		logger.info(req_data["statuses"])
 	return response
 
+
 @app.route("/message", methods=['POST'])
 def on_omni_message():
 	"""Receives messages from Omni and uses the appropriate method to send them to Hermes.
@@ -119,16 +119,16 @@ def on_omni_message():
 
 			Expects the JSON to be like:
 			{
-			    "recipient": "+5585996361001",
-			    "type": "text",
-			    "content": "olar"
+				"recipient": "+5585996361001",
+				"type": "text",
+				"content": "olar"
 			}
 
-		    Returns:
-		      The sucess of error from omni request
+			Returns:
+			  The sucess of error from omni request
 		"""
 	req_data = request.get_json()
-	
+
 	if req_data.get('media_type'):
 		return send_attachment_message(req_data)
 	elif req_data.get('message'):
@@ -164,15 +164,15 @@ def send_omni_message(payload):
 					Args:
 						payload: the message to be sendo to omni. e.g:
 						{
-						    "sender": "5585996361064",
-						    "recipient": 558588886522,
-						    "send_date": "2019/03/05 13:15",
-						    "content": "Hello",
-						    "message_id": "5585996361064s",
-						    "plataform_name": "whatsapp_business"
+							"sender": "5585996361064",
+							"recipient": 558588886522,
+							"send_date": "2019/03/05 13:15",
+							"content": "Hello",
+							"message_id": "5585996361064s",
+							"plataform_name": "whatsapp_business"
 						}
-				    Returns:
-				      The sucess of error from the request made to omni
+					Returns:
+					  The sucess of error from the request made to omni
 		"""
 	request_endpoint = omni_link + ':' + omni_port + '/api/message'
 	response = requests.post(
@@ -181,14 +181,15 @@ def send_omni_message(payload):
 		headers={'Content-Type': 'application/json'})
 	return response.content
 
+
 def send_text_message(omni_message):
 	message = json.loads('{}')
 	message["message"] = {}
 	message["message"].update({"to": format_phone(omni_message["recipient"]),
-	                           "type": "text",
-	                           "text": omni_message["content"],
-	                           "media_id": media_id,
-	                           "recipient_type": "individual"})
+							   "type": "text",
+							   "text": omni_message["content"],
+							   "media_id": media_id,
+							   "recipient_type": "individual"})
 
 	logger.debug(f"send_text_message -> message: {message}")
 
@@ -197,6 +198,7 @@ def send_text_message(omni_message):
 	logger.debug(f"send_text_message -> response: {response}")
 
 	return response
+
 
 def send_hsm_message(omni_message):
 	# message = json.loads('{}')
@@ -215,6 +217,7 @@ def send_hsm_message(omni_message):
 
 	return response
 
+
 def send_attachment_message(omni_message):
 	message = json.loads('{}')
 	message["message"] = {}
@@ -229,8 +232,8 @@ def send_attachment_message(omni_message):
 							   "content_type": omni_message["media_type"],
 							   "media_id": media_id,
 							   "url": url,
-							   "caption":  omni_message["media_name"],
-	                           "recipient_type": "individual"})
+							   "caption": omni_message["media_name"],
+							   "recipient_type": "individual"})
 	if "caption" in omni_message:
 		message["message"].update({"caption": omni_message["caption"]})
 
@@ -241,6 +244,7 @@ def send_attachment_message(omni_message):
 	logger.debug(f"send_attachment_message -> response: {response}")
 
 	return response
+
 
 def get_type(myme):
 	type = myme.split('/')[0]
@@ -253,15 +257,19 @@ def get_type(myme):
 def format_phone(phone_number):
 	return "+" + phone_number
 
+
 def save_file(data, file_location):
 	with open(file_location, "wb") as fh:
 		fh.write(base64.decodebytes(data))
 
+
 def download_file(url):
 	return base64.b64encode(requests.get(url).content).decode("utf-8")
 
+
 def create_hsm(hsm_name, hsm_message):
 	return bot.create_hsm(hsm_name, hsm_message)
+
 
 if __name__ == "__main__":
 	app.run(threaded=True, host='0.0.0.0')
